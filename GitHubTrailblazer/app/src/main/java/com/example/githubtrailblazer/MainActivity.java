93bae2759a981e433d66cfc22e38872b65b18439
AlexPawelczyk <alex.pawelczyk@uwaterloo.ca>
2020-06-06 15:32:28 -0400

    Add on-click function for user logout

--- GitHubTrailblazer/app/src/main/java/com/example/githubtrailblazer/MainActivity.java
+++ GitHubTrailblazer/app/src/main/java/com/example/githubtrailblazer/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.githubtrailblazer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity
+{
+    // define UI variables
+    private Button mLogoutBtn;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // assign UI variables to UI elements
+        mLogoutBtn = findViewById(R.id.main_logout_btn);
+
+        // on-click listener for logging user out and sending them to login activity
+        mLogoutBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
