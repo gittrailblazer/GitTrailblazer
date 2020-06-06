663eb4c08e9777213468f6926ab3c068d92e452c
AlexPawelczyk <alex.pawelczyk@uwaterloo.ca>
2020-06-06 12:17:42 -0400

    Add on-click functions for register/login buttons

--- GitHubTrailblazer/app/src/main/java/com/example/githubtrailblazer/InitialActivity.java
+++ GitHubTrailblazer/app/src/main/java/com/example/githubtrailblazer/InitialActivity.java
@@ -2,13 +2,46 @@ package com.example.githubtrailblazer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class InitialActivity extends AppCompatActivity {
+public class InitialActivity extends AppCompatActivity
+{
+    // define UI variables
+    private Button mRegisterBtn, mLoginBtn;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial);
+
+        // assign UI variables to UI elements
+        mRegisterBtn = findViewById(R.id.initial_register_btn);
+        mLoginBtn = findViewById(R.id.initial_login_btn);
+
+        // on-click listener for sending user to registration activity
+        mRegisterBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(InitialActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // on-click listener for sending user to login activity
+        mLoginBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(InitialActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
