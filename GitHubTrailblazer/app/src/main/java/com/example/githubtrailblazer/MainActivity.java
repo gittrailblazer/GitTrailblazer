b195aa95279beea0bdbf64b62871caab85669399
kejianZ <33974109+kejianZ@users.noreply.github.com>
2020-06-13 17:36:57 -0400

    Delete userinfo function in MainActivity

--- GitHubTrailblazer/app/src/main/java/com/example/githubtrailblazer/MainActivity.java
+++ GitHubTrailblazer/app/src/main/java/com/example/githubtrailblazer/MainActivity.java
@@ -8,11 +8,15 @@ import android.view.View;
 import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity
 {
     // define UI variables
     private Button mLogoutBtn;
+    private Button mDeleAccountBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -22,6 +26,7 @@ public class MainActivity extends AppCompatActivity
 
         // assign UI variables to UI elements
         mLogoutBtn = findViewById(R.id.main_logout_btn);
+        mDeleAccountBtn = findViewById(R.id.main_deleAccount_btn);
 
         // on-click listener for logging user out and sending them to login activity
         mLogoutBtn.setOnClickListener(new View.OnClickListener()
@@ -31,6 +36,17 @@ public class MainActivity extends AppCompatActivity
             {
                 FirebaseAuth.getInstance().signOut();
                 Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // on-click listener for deleting user account and sending them to login activity
+        mDeleAccountBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                user.delete();
+                Intent intent = new Intent(MainActivity.this, InitialActivity.class);
                 startActivity(intent);
             }
         });
